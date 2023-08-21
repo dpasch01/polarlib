507eb78abaed099eaed8a820a2aaa7fda58e7a98
Demetris Paschalides <dpasch01@cs.ucy.ac.cy>
2023-08-21 17:28:10 +0000

    Lowercase package name.

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh: long_description = fh.read()
 with open('./requirements.txt') as f: reqs = f.read().strip().split('\n')
 
 setuptools.setup(
-    name                          = "POLARLib",
+    name                          = "polarlib",
     version                       = "0.0.1",
     author                        = "Demetris Paschalides",
     author_email                  = "dpasch01@ucy.ac.cy",
-    description                   = "N/A",
+    description                   = "Python library for POLAR, a framework for the modelling of polarization and identification of polarizing topics in news articles.",
     long_description              = long_description,
     long_description_content_type = "text/markdown",
     packages                      = setuptools.find_packages(where="src"),
