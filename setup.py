a09e8ffbff974259f48b371cd2f1bb7be4807327
Demetris Paschalides <dpasch01@cs.ucy.ac.cy>
2023-08-21 16:58:39 +0000

    Added __init__.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 import setuptools
 
-with open("README.md", "r", encoding = "utf-8") as fh: long_description = fh.read()
+with open("README.md", "r") as fh: long_description = fh.read()
+
+with open('./requirements.txt') as f: reqs = f.read().strip().split('\n')
 
 setuptools.setup(
-    name                          = "polarlib",
+    name                          = "POLARLib",
     version                       = "0.0.1",
     author                        = "Demetris Paschalides",
     author_email                  = "dpasch01@ucy.ac.cy",
     description                   = "N/A",
     long_description              = long_description,
     long_description_content_type = "text/markdown",
-    package_dir                   = {"": "polarlib"},
     packages                      = setuptools.find_packages(where="polarlib"),
-    python_requires               = ">=3.9"
+    package_dir                   = {'': 'polarlib'},
+    python_requires               = '>=3.9',
+    install_requires              = reqs
 )
